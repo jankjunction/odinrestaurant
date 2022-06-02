b4b30cbdacd004d976b5252987882fd424f05a10
Micah Bell <micahjoshuabell@gmail.com>
2022-06-02 13:21:55 -0400

    Add header.js build functionality

---
+++ src/header.js
@@ -0,0 +1,33 @@
+import Icon from './img/reddit.png';
+
+const buildHeader = (() => {
+    let content = document.getElementById('content');
+    let header = document.createElement('div');
+    let redditIcon = document.createElement('img');
+    let logoText = document.createElement('span');
+    let nav = document.createElement('div');
+    let home = document.createElement('span');
+    let menu = document.createElement('span');
+    let contact = document.createElement('span');
+    
+    logoText.textContent = 'redditstaurant';
+    home.textContent = 'home';
+    menu.textContent = 'menu';
+    contact.textContent = 'contact';
+
+    logoText.setAttribute('id', 'logoText');
+    redditIcon.setAttribute('src', Icon);
+    redditIcon.setAttribute('id', 'icon');
+    header.setAttribute('id', 'header');
+    nav.setAttribute('id', 'nav');
+    
+    content.appendChild(header);
+    
+    header.appendChild(redditIcon);
+    header.appendChild(logoText);
+    header.appendChild(nav);
+
+    nav.appendChild(home);
+    nav.appendChild(menu);
+    nav.appendChild(contact);
+})();
---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './style.css';
+import Header from './header.js';

29092c2e7c512055c474b8b9ad8cd25b232c42e9
Micah Bell <micahjoshuabell@gmail.com>
2022-06-03 12:58:30 -0400

    Tweak Contact Styling

--- src/contact.js
+++ src/contact.js
@@ -26,7 +26,7 @@ const buildContact = (() => {
     yelpIcon.setAttribute('src', yelp);
 
     let map = document.createElement('div');
-    map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d97949.59234237966!2d-86.13724160000001!3d39.8983168!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886b539bf1fa5401%3A0x276b79e279cf5483!2sHolliday%20Park!5e0!3m2!1sen!2sus!4v1654273260317!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+    map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d97949.59234237966!2d-86.13724160000001!3d39.8983168!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886b539bf1fa5401%3A0x276b79e279cf5483!2sHolliday%20Park!5e0!3m2!1sen!2sus!4v1654273260317!5m2!1sen!2sus" width="600" height="450" id="map-frame" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
 
 
     content.appendChild(contact);
